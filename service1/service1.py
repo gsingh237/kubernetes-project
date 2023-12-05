3d22fa2613656149161f872df57bf5eecfea3de9
gsingh237 <gurpartap.singh@coforge.com>
2023-12-05 15:48:56 +0530

    Initial commit

---
+++ service1/service1.py
@@ -0,0 +1,11 @@
+#service1.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/path1')
+def service1():
+    return "Hello, This is Service1 Response!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
---
+++ service2/service2.py
@@ -0,0 +1,11 @@
+#service2.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/path2')
+def service2():
+    return "Hello, This is Service2 Response!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
